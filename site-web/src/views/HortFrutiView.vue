6331a2357d4d3d91c097ec2d83e272c15cc37e33
dettmann32 <136387394+dettmann32@users.noreply.github.com>
2023-12-19 15:07:25 -0300

    att hortifrut

--- site-web/src/views/HortFrutiView.vue
+++ site-web/src/views/HortFrutiView.vue
@@ -1,38 +1,79 @@
 <template>
   <div>
-    <section class="flex justify-center w-full md:h-[80vh] h-[55vh]  flex-col bg-cover">
-      <h1 class="text-center text-2xl mt-4">Hortifrúti Qualidade e Exposição</h1>
-      <div class="h-full w-full flex justify-center ">
-  
-        <div class=" w-[100%] h-[80%]   md:w-[50%] md:h-[100%] ">
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/KxvohHyqtRE" title="Hortifruti qualidade e exposição" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    <section class="flex justify-center w-full  flex-col bg-cover">
+      <h1  class="text-center text-[4vw] md:mt-4 sm:mb-5 sm:mt-10 mt-3">Hortifrúti Qualidade e Exposição</h1>
+      <div class="md:h-[50vw] w-[100vw] flex  justify-center ">
+
+        <div class=" h-[40vw] w-[40vw]    ">
+          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/KxvohHyqtRE"
+            title="Hortifruti qualidade e exposição" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen>
 
           </iframe>
         </div>
+        <div class=" w-[50vw] h-[80vw]   md:w-[50vw] md:h-[40vw]  mr-10 md:text-[1.5vw] md:ml-6 text-[2.8vw] mb-2">
+          <p>
+            A padaria e confeitaria têm participação de 12% nas vendas totais, em média, nos supermercados.
+             Estes setores tornam-se obrigatórios nas empresas modernas, 
+            atraindo mais clientes e proporcionando rentabilidade para o setor.
+            <span class="text-justify">
+
+              Neste curso serão abordado de maneira prática e adequada:</span>
+            <br><br>
+          </p>
+          <ul>
+            Neste curso abordaremos:
+            <li>- Origem e conceito do supermercado;</li>
+            <li>- Visita à loja;</li>
+            <li>- O que mais se vende em supermercado;</li>
+            <li>- Requisitos para ser Repositor de supermercados;</li>
+            <li>- Apresentação e atitudes do Repositor;</li>
+            <li>- A importância do Repositor;</li>
+            <li>- Principais atribuições do Repositor;</li>
+            <li>- Características de uma boa exposição;</li>
+            <li>- Repositor do Açougue;</li>
+            <li>- Repositor de Laticínios;</li>
+            <li>- Repositor de Hortifrutigranjeiros;</li>
+            <li>- Repositor de Mercearia;</li>
+            <li>- Repositor de Não Alimentos;</li>
+            <li>- Área de depósito.</li>
+
+
+          </ul>
+          <br>
+
+
+
+
+        </div>
       </div>
-  
+
     </section>
     <section class="flex justify-center w-full md:h-[80vh]  flex-col bg-cover mb-10 h-[55vh]">
-      <h1 class="text-center text-2xl mt-4">Hortifrúti no Supermercado</h1>
+      <h1 class="text-center text-2xl mt-2">Hortifrúti no Supermercado</h1>
       <div class="h-full w-full flex justify-center ">
-  
+
         <div class=" w-[100%] h-[80%]   md:w-[50%] md:h-[100%] ">
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/rJ9QfE1tmXQ" title="Hortifruti no supermercado" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/rJ9QfE1tmXQ"
+            title="Hortifruti no supermercado" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen>
 
           </iframe>
         </div>
       </div>
-  
+
     </section>
-    </div>
-  </template>
+  </div>
+</template>
   
-  <script setup>
+<script setup>
+
+</script>
   
-  </script>
   
-  
-  <style scoped lang="sass">
+<style scoped lang="sass">
   
   section
       background-image: url('https://dettmann32.github.io/imagens-for-supershow-web-site/imagens/texturaFk.png')
