5137c0c5e17bc14aceed854123527ec48fcf90ca
dettmann32 <136387394+dettmann32@users.noreply.github.com>
2023-12-21 09:04:07 -0300

    hortifruti

--- site-web/src/views/HortFrutiView.vue
+++ site-web/src/views/HortFrutiView.vue
@@ -1,69 +1,72 @@
 <template>
-  <div>
-    <section class="flex justify-center w-full  flex-col bg-cover">
-      <h1  class="text-center text-[4vw] md:mt-4 sm:mb-5 sm:mt-10 mt-3">Hortifrúti Qualidade e Exposição</h1>
-      <div class="md:h-[50vw] w-[100vw] flex  justify-center ">
+  <div class="h-[230vw] md:h-[110vw]" id="background">
+    <div class="flex flex-col sm:flex-row justify-center gap-10 ">
+      <section class=" bg-cover ">
+        <h1 class="text-center text-[2vw] md:mt-4 sm:mb-5 sm:mt-10 mt-3 ">Hortifrúti Qualidade e Exposição</h1>
+        <div class="md:h-[50vw]  flex  justify-center ">
 
-        <div class=" h-[40vw] w-[40vw]    ">
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/KxvohHyqtRE"
-            title="Hortifruti qualidade e exposição" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen>
+          <div class=" h-[40vw] w-[45vw]">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/KxvohHyqtRE"
+              title="Hortifruti qualidade e exposição" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen>
 
-          </iframe>
+            </iframe>
+          </div>
         </div>
-        <div class=" w-[50vw] h-[80vw]   md:w-[50vw] md:h-[40vw]  mr-10 md:text-[1.5vw] md:ml-6 text-[2.8vw] mb-2">
-          <p>
-            A padaria e confeitaria têm participação de 12% nas vendas totais, em média, nos supermercados.
-             Estes setores tornam-se obrigatórios nas empresas modernas, 
-            atraindo mais clientes e proporcionando rentabilidade para o setor.
-            <span class="text-justify">
+      </section>
+      <section class="bg-cover ">
+        <h1 class="text-center text-[2vw] md:mt-4 sm:mb-5 sm:mt-10 mt-3">Hortifrúti no Supermercado</h1>
+        <div class="md:h-[50vw]  flex  justify-center ">
 
-              Neste curso serão abordado de maneira prática e adequada:</span>
-            <br><br>
-          </p>
-          <ul>
-            Neste curso abordaremos:
-            <li>- Origem e conceito do supermercado;</li>
-            <li>- Visita à loja;</li>
-            <li>- O que mais se vende em supermercado;</li>
-            <li>- Requisitos para ser Repositor de supermercados;</li>
-            <li>- Apresentação e atitudes do Repositor;</li>
-            <li>- A importância do Repositor;</li>
-            <li>- Principais atribuições do Repositor;</li>
-            <li>- Características de uma boa exposição;</li>
-            <li>- Repositor do Açougue;</li>
-            <li>- Repositor de Laticínios;</li>
-            <li>- Repositor de Hortifrutigranjeiros;</li>
-            <li>- Repositor de Mercearia;</li>
-            <li>- Repositor de Não Alimentos;</li>
-            <li>- Área de depósito.</li>
+          <div class=" h-[40vw] w-[45vw]">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/rJ9QfE1tmXQ"
+              title="Hortifruti no supermercado" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen>
 
-
-          </ul>
-          <br>
-
-
-
-
+            </iframe>
+          </div>
         </div>
+
+      </section>
+    </div>
+    <section>
+      <div class=" w-[50vw] h-[80vw]   md:w-[50vw] md:h-[40vw]  mr-10 md:text-[1.5vw] md:ml-6 text-[2.8vw] mb-2">
+        <p>
+          A padaria e confeitaria têm participação de 12% nas vendas totais, em média, nos supermercados.
+          Estes setores tornam-se obrigatórios nas empresas modernas,
+          atraindo mais clientes e proporcionando rentabilidade para o setor.
+          <span class="text-justify">
+
+            Neste curso serão abordado de maneira prática e adequada:</span>
+          <br><br>
+        </p>
+        <ul>
+          Neste curso abordaremos:
+          <li>- Origem e conceito do supermercado;</li>
+          <li>- Visita à loja;</li>
+          <li>- O que mais se vende em supermercado;</li>
+          <li>- Requisitos para ser Repositor de supermercados;</li>
+          <li>- Apresentação e atitudes do Repositor;</li>
+          <li>- A importância do Repositor;</li>
+          <li>- Principais atribuições do Repositor;</li>
+          <li>- Características de uma boa exposição;</li>
+          <li>- Repositor do Açougue;</li>
+          <li>- Repositor de Laticínios;</li>
+          <li>- Repositor de Hortifrutigranjeiros;</li>
+          <li>- Repositor de Mercearia;</li>
+          <li>- Repositor de Não Alimentos;</li>
+          <li>- Área de depósito.</li>
+
+
+        </ul>
+        <br>
+
+
+
+
       </div>
-
-    </section>
-    <section class="flex justify-center w-full md:h-[80vh]  flex-col bg-cover mb-10 h-[55vh]">
-      <h1 class="text-center text-2xl mt-2">Hortifrúti no Supermercado</h1>
-      <div class="h-full w-full flex justify-center ">
-
-        <div class=" w-[100%] h-[80%]   md:w-[50%] md:h-[100%] ">
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/rJ9QfE1tmXQ"
-            title="Hortifruti no supermercado" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen>
-
-          </iframe>
-        </div>
-      </div>
-
     </section>
   </div>
 </template>
@@ -75,7 +78,7 @@
   
 <style scoped lang="sass">
   
-  section
+  #background
       background-image: url('https://dettmann32.github.io/imagens-for-supershow-web-site/imagens/texturaFk.png')
   
   </style>
