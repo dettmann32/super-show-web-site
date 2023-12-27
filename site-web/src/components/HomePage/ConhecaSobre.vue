eaecc3f63a70d5754bdf2e95b216214e2c02b51a
dettmann32 <136387394+dettmann32@users.noreply.github.com>
2023-12-27 10:18:41 -0300

    novo texto da historia do super show no conheça sobre

--- site-web/src/components/HomePage/ConhecaSobre.vue
+++ site-web/src/components/HomePage/ConhecaSobre.vue
@@ -1,34 +1,41 @@
 <template>
-    <section class="m-auto flex  flex-col px-5 justify-center  items-center bg-cover w-[100vw]  shadow-xl  lg:flex-row py-5" style="background-image: url('https://static.wixstatic.com/media/8a9ccb_230081661a7244819b6b3f14d4fa8ab0~mv2.jpg/v1/fill/w_1151,h_863,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/8a9ccb_230081661a7244819b6b3f14d4fa8ab0~mv2.jpg')" id="sobrenos">
-        <div class="w-[100%] flex flex-col items-center bg-white p-2 shadow-2xl rounded-xl lg:h-[600px]" >
-            <h1 class="text-red-700 text-[300%]">
-                Grupo FK
+    <section class="m-auto flex  flex-col px-5 justify-center  items-center bg-cover w-[100vw]  shadow-xl  lg:flex-row py-5"
+        style="background-image: url('https://static.wixstatic.com/media/8a9ccb_230081661a7244819b6b3f14d4fa8ab0~mv2.jpg/v1/fill/w_1151,h_863,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/8a9ccb_230081661a7244819b6b3f14d4fa8ab0~mv2.jpg')"
+        id="sobrenos">
+        <div class="w-[100%] flex flex-col items-center bg-white  p-2 px-6   shadow-2xl rounded-xl ">
+            <h1 class="text-red-700 text-[300%] lg:text-[400%]">
+                SuperShow
             </h1>
-            <h2 class="text-blue-600 text-[150%] ">
+            <h2 class="text-blue-600 text-[150%] lg:text-[200%]">
                 Conheça um pouco mais sobre nós!
             </h2>
 
-            <p class="text-justify text-[120%] lg:text-[135%]" >
-                O SuperShow Supermercados é uma empresa com mais de 22 anos de história, com cinco lojas presentes no estado
-                do Espírito Santo.<br><br>
+            <p class="text-justify text-[120%] lg:text-[180%]">
+                A história do SUPER SHOW SUPERMERCADOS se confundi com a história da família, ela sempre se desenvolveu a
+                partir dos valores familiares, enraizados nas rotinas de trabalho de cada um de nossos colaboradores e com o
+                compromisso com a sociedade.<br><br>
 
 
-                Sua primeira loja foi aberta em 2001, em Santa Maria de Jetibá, e desde então tem recebido e entregado
-                produtos de qualidade aos clientes.<br><br>
+                Atualmente a empresa conta com mais de 200 colaboradores, em 5 lojas e 2 depósitos<br><br>
 
 
-                As lojas oferecem uma ampla estrutura, incluindo padaria com produtos diversos, hortifruti com frutas e
-                legumes frescos, e açougue com as melhores carnes.<br><br>
+            <ul class="list-disc  text-[120%] lg:text-[180%] lg:ml-2">
+                <li>No ano de 2010 primeira filial no bairro São Luiz</li>
+                <li>Em 2011 bairro Vila Nova</li>
+                <li>Em 2013 investimos em uma loja ao centro do bairro rural de Alto Rio Possmoser</li>
+                <li>E em 2017 chegou a vez de São João do Garrafão</li>
+            </ul><br><br>
 
 
-                Para maior comodidade, a empresa realiza entregas das compras dos clientes, tornando a experiência de compra
-                ainda mais vantajosa no SuperShow Supermercados.<br><br>
+          <span class="text-[120%] lg:text-[180%] text-justify"> O foco estratégico da empresa é ser referência em melhor preço, melhor qualidade de produtos e melhor atendimento
+            na região Central Serrana do Espírito Santo, sendo assim ainda temos um grande desafia de conquistar novar áreas
+            dentro de fora de Santa Maria de Jetibá.</span> <br><br>
 
             </p>
         </div>
 
 
-      
+
 
         <CarouselSobre></CarouselSobre>
 
@@ -44,12 +51,12 @@
 
             <div>
                 <div><img src="../../assets/images/2.webp" alt=""></div>
-                <div class="text-center text-white"><span> 5 Lojas<br>  no Municipio </span></div>
+                <div class="text-center text-white"><span> 5 Lojas<br> no Municipio </span></div>
             </div>
 
             <div>
-               <div> <img src="../../assets/images/3-removebg-preview (1).webp" alt=""></div>
-               <div class="text-center text-white"><span>+20 Anos <br> de Comercio </span></div>
+                <div> <img src="../../assets/images/3-removebg-preview (1).webp" alt=""></div>
+                <div class="text-center text-white"><span>+20 Anos <br> de Comercio </span></div>
             </div>
 
         </div>
